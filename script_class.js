d0afc6c8b8d5fd12cfe6fdc3fafbecadeff52872
JuriKovtun <kovtun.yurko@gmail.com>
2018-02-27 14:47:31 +0200

    initial commit

--- script_class.js
+++ script_class.js
@@ -5,10 +5,14 @@ let attrSrc = "src";
 let srcValue = "disk/file.jpg";
 let attrAlt = "alt";
 let altValue = "the picture";
+let innerHtmlValue = "here I am!";
 
 let product = document.createElement(elementName);
-product.setAttribute(attrClass,classValue);
-product.setAttribute(attrSrc,srcValue);
-product.setAttribute(attrAlt,altValue);
+product.setAttribute(attrClass, classValue);
+product.setAttribute(attrSrc, srcValue);
+product.setAttribute(attrAlt, altValue);
+product.innerHTML = `${innerHtmlValue}  <br>  my class: ${classValue}`;
+
+document.body.appendChild(product);
 
 console.log(product);

45e5c17ffbd072554763e4732af21c468df1fc63
JuriKovtun <kovtun.yurko@gmail.com>
2018-02-27 14:13:47 +0200

    initial commit

---
+++ script_class.js
@@ -0,0 +1,10 @@
+let product = document.createElement("div");
+// product.classList.add("newClass");
+
+product.setAttribute("src", "disk/file.jpg");
+product.setAttribute("class","attrClass");
+product.classList.add("addClass");
+// a = product.getAttribute("src");
+
+
+console.log(product);

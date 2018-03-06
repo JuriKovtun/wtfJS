135c2c0da52627b0d1fb10fa60dd579b1a17a7e5
JuriKovtun <kovtun.yurko@gmail.com>
2018-03-06 11:00:37 +0200

    product builder

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-const database = ["Caption Text", "image url", "Ingredients", "price"];
+const database = [{class: "coockie", captionTag: "h4", captionText: "Plätzchen 001", imageAdress: "img/product_pics/cookies/cookies001.jpg", ingredients: "Lorem ipsum dolor sit amet, his oratio ancillae ea, vidit malis eirmod eos eu.", price: "111"},
+                  {class: "coockie", captionTag: "h4", captionText: "Plätzchen 002", imageAdress: "img/product_pics/cookies/cookies002.jpg", ingredients: "Lorem ipsum dolor sit amet, his oratio ancillae ea, vidit malis eirmod eos eu.", price: "222"},
+                  {class: "jam", captionTag: "h4", captionText: "Marmelade 001", imageAdress: "img/product_pics/jam/jam001.jpg", ingredients: "Lorem ipsum dolor sit amet, his oratio ancillae ea, vidit malis eirmod eos eu.", price: "333"},
+                  {class: "jam", captionTag: "h4", captionText: "Marmelade 002", imageAdress: "img/product_pics/jam/jam002.jpg", ingredients: "Lorem ipsum dolor sit amet, his oratio ancillae ea, vidit malis eirmod eos eu.", price: "444"},
+];
 
 let productTemplate = database;
 
-for (let i = 0; i < productTemplate.length; i++) {
-    let product = document.createElement("div");
-    product.classList.add("product");
-    product.innerHTML = productTemplate[i];
-    document.body.appendChild(product);
+let className = "coockie";
+let CaptionTag = "h4";
 
-    console.log(product);
-}
+// for (let i = 0; i < productTemplate.length; i++) {
+//     let product = document.createElement("div");
+//     product.classList.add("product");
+//     product.innerHTML = productTemplate[i];
+//     document.body.appendChild(product);
+//
+//     console.log(product);
+// }
 
 
 
--- script_class.js
+++ script_class.js
@@ -1,19 +1,46 @@
-let tagName = "div";
-let attrClass = "class";
-let classValue = "newClass";
+const database = [{className: "cookie", captionTag: "h4", captionText: "Plätzchen 001", imageAdress: "img/product_pics/cookies/cookies001.jpg", ingredients: "Lorem ipsum dolor sit amet, his oratio ancillae ea, vidit malis eirmod eos eu.", price: "111"},
+    {className: "cookie", captionTag: "h4", captionText: "Plätzchen 002", imageAdress: "img/product_pics/cookies/cookies002.jpg", ingredients: "Lorem ipsum dolor sit amet, his oratio ancillae ea, vidit malis eirmod eos eu.", price: "222"},
+    {className: "jam", captionTag: "h4", captionText: "Marmelade 001", imageAdress: "img/product_pics/jam/jam001.jpg", ingredients: "Lorem ipsum dolor sit amet, his oratio ancillae ea, vidit malis eirmod eos eu.", price: "333"},
+    {className: "jam", captionTag: "h4", captionText: "Marmelade 002", imageAdress: "img/product_pics/jam/jam002.jpg", ingredients: "Lorem ipsum dolor sit amet, his oratio ancillae ea, vidit malis eirmod eos eu.", price: "444"},
+];
+
+let tagName = "h4";
+let classValue = "cookie";
 let attrSrc = "src";
-let srcValue = "disk/file.jpg";
+let srcValue = "img/product_pics/cookies/cookies001.jpg";
 let attrAlt = "alt";
-let altValue = "the picture";
+let altValue = "The tasty cookie!";
 let innerHtmlValue = "here I am!";
+let ingredients = "many of something";
+let priceValue = "111";
 
-let product = document.createElement(tagName);
-product.setAttribute(attrClass, classValue);
-product.setAttribute(attrSrc, srcValue);
-product.setAttribute(attrAlt, altValue);
-product.innerHTML = `${innerHtmlValue}<br>my class: ${classValue}`;
+let product = document.createElement("div");
+product.classList.add("product__item");
 
+let productCaption = document.createElement(tagName);
+productCaption.classList.add(classValue);
+productCaption.innerHTML = `${innerHtmlValue}<br>my class: ${classValue}`;
+
+let productImage = document.createElement("img");
+productImage.classList.add(classValue);
+productImage.setAttribute(attrSrc, srcValue);
+productImage.setAttribute(attrAlt, altValue);
+
+let productIngredients = document.createElement("span");
+productIngredients.classList.add(classValue);
+productIngredients.innerHTML = `Ingredients: ${ingredients}`;
+
+let productPrice = document.createElement("span");
+productPrice.classList.add(classValue);
+productPrice.innerHTML = `Price: ${priceValue}`;
+
+
+product.appendChild(productCaption);
+product.appendChild(productImage);
+product.appendChild(productIngredients);
+product.appendChild(productPrice);
 document.body.appendChild(product);
 
 console.log(product);
-alert(product);
+
+

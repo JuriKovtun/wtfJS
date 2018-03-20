ef269da799e670809f9cb7cee5d3e16202ecefcf
matvii.kovtun <kovtun_m@ucu.edu.ua>
2018-03-20 14:12:52 +0200

    Added array with different categories.

--- dubbingRemove/script.js
+++ dubbingRemove/script.js
@@ -1,7 +1,7 @@
-const arr = ["d","a","b","a","c"];
+const arr = ["d", "a", "b", "a", "c"];
 let newArr = [];
 
-for (let i=0; i<arr.length; i++) {
+for (let i = 0; i < arr.length; i++) {
     if (newArr.indexOf(arr[i]) === -1) {
         newArr.push(arr[i]);
         console.log(arr[i]);
@@ -9,5 +9,19 @@ for (let i=0; i<arr.length; i++) {
 }
 
 console.log(newArr.sort());
+
+
+const data = [{
+        category: "a"
+    }, {
+        category: "b"
+    }, {
+        category: "c"
+    }, {
+        category: "a"
+}];
+
+const categories = [...new Set(data.map((el) => el.category))];
+console.log(categories);
 
 // omg push
